820155bc1f7d55fc3f956f9a0c752fbe0386e2ab
IvanVarela <tech@ipndigital.com>
2020-09-28 18:24:11 -0500

    Changers in Register form ussing widget attributes

--- camaro/console/views.py
+++ camaro/console/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django import forms
+
+
+class NewRegisterForm(forms.Form):
+    firstName = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control',
+                                                              'placeholder': 'Nombres'}))
+    lastName = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control',
+                                                             'placeholder': 'Apellidos'}))
+    alias = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control',
+                                                          'placeholder': 'Alias'
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control',
+                                                            'placeholder': 'Correo Electónico'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                 'placeholder': 'Password',
+                                                                 'type': 'password'}))
+
 
 # Create your views here.
 
@@ -8,5 +25,6 @@ def index(request):
 
 
 def register(request):
-    return render(request, "console/register.html")
-
+    return render(request, "console/register.html", {
+        "form": NewRegisterForm()
+    })
